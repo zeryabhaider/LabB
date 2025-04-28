76ab1bf97f11c6dbe0f1596eea08b6701638818d
lucav <luca.vi003.lv@gmail.com>
2025-04-28 11:03:49 +0200

    primo commit: prima versione della grafica delle pagine

---
+++ src/main/java/theknife/TheKnife.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package theknife;
+
+/**
+ *
+ * @author lucav
+ */
+public class TheKnife{
+
+    public static void main(String[] args) {
+        new TheKnifeHome().setVisible(true);
+    }
+}
